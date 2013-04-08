ae5e14a5113acd65e19f4c47f3b2d5cd5235c3b7
Jay Jagpal <jay@strangemother.com>
2013-04-08 09:40:59 +0100

    colors example

--- examples/car/examples.js
+++ examples/car/examples.js
@@ -1,8 +1,136 @@
-weightCog = function(){
-    $(function() {
-        for (var i = 0; i < 2; i++) {
-            addRadii(i);
+function rgb2hex(color) {
+    if (color.substr(0, 1) === '#') {
+        return color;
+    }
+    var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
+    
+    var red = parseInt(digits[2]);
+    var green = parseInt(digits[3]);
+    var blue = parseInt(digits[4]);
+    
+    var rgb = blue | (green << 8) | (red << 16);
+    return digits[1] + '#' + rgb.toString(16);
+};
+
+function RGB2HSV(rgb) {
+    hsv = new Object();
+    max=max3(rgb.r,rgb.g,rgb.b);
+    dif=max-min3(rgb.r,rgb.g,rgb.b);
+    hsv.saturation=(max==0.0)?0:(100*dif/max);
+    if (hsv.saturation==0) hsv.hue=0;
+    else if (rgb.r==max) hsv.hue=60.0*(rgb.g-rgb.b)/dif;
+    else if (rgb.g==max) hsv.hue=120.0+60.0*(rgb.b-rgb.r)/dif;
+    else if (rgb.b==max) hsv.hue=240.0+60.0*(rgb.r-rgb.g)/dif;
+    if (hsv.hue<0.0) hsv.hue+=360.0;
+    hsv.value=Math.round(max*100/255);
+    hsv.hue=Math.round(hsv.hue);
+    hsv.saturation=Math.round(hsv.saturation);
+    return hsv;
+}
+
+function rgb2hsv () {
+    var rr, gg, bb,
+        r = arguments[0] / 255,
+        g = arguments[1] / 255,
+        b = arguments[2] / 255,
+        h, s,
+        v = Math.max(r, g, b),
+        diff = v - Math.min(r, g, b),
+        diffc = function(c){
+            return (v - c) / 6 / diff + 1 / 2;
         };
-        ready=true;
+
+    if (diff == 0) {
+        h = s = 0;
+    } else {
+        s = diff / v;
+        rr = diffc(r);
+        gg = diffc(g);
+        bb = diffc(b);
+
+        if (r === v) {
+            h = bb - gg;
+        }else if (g === v) {
+            h = (1 / 3) + rr - bb;
+        }else if (b === v) {
+            h = (2 / 3) + gg - rr;
+        }
+        if (h < 0) {
+            h += 1;
+        }else if (h > 1) {
+            h -= 1;
+        }
+    }
+    return {
+        h: Math.round(h * 360),
+        s: Math.round(s * 100),
+        v: Math.round(v * 100),
+        hue: Math.round(h * 360),
+        saturation: Math.round(s * 100),
+        value: Math.round(v * 100)
+    };
+}
+
+// RGB2HSV and HSV2RGB are based on Color Match Remix [http://color.twysted.net/]
+// which is based on or copied from ColorMatch 5K [http://colormatch.dk/]
+function HSV2RGB(hsv) {
+    var rgb=new Object();
+    if (hsv.saturation==0) {
+        rgb.r=rgb.g=rgb.b=Math.round(hsv.value*2.55);
+    } else {
+        hsv.hue/=60;
+        hsv.saturation/=100;
+        hsv.value/=100;
+        i=Math.floor(hsv.hue);
+        f=hsv.hue-i;
+        p=hsv.value*(1-hsv.saturation);
+        q=hsv.value*(1-hsv.saturation*f);
+        t=hsv.value*(1-hsv.saturation*(1-f));
+        switch(i) {
+        case 0: rgb.r=hsv.value; rgb.g=t; rgb.b=p; break;
+        case 1: rgb.r=q; rgb.g=hsv.value; rgb.b=p; break;
+        case 2: rgb.r=p; rgb.g=hsv.value; rgb.b=t; break;
+        case 3: rgb.r=p; rgb.g=q; rgb.b=hsv.value; break;
+        case 4: rgb.r=t; rgb.g=p; rgb.b=hsv.value; break;
+        default: rgb.r=hsv.value; rgb.g=p; rgb.b=q;
+        }
+        rgb.r=Math.round(rgb.r*255);
+        rgb.g=Math.round(rgb.g*255);
+        rgb.b=Math.round(rgb.b*255);
+    }
+    return rgb;
+}
+
+hsv2hex = function(h,s,v){
+    var rgbo = HSV2RGB({
+        hue: h,
+        saturation: s,
+        value: v
+    })
+
+    return rgb2hex('rgb(' +  rgbo.r + ', ' + rgbo.g + ', ' + rgbo.b + ')')
+
+}
+
+
+radialDemo = function(){
+    $(function() {
+        var c = 150;
+        var saturation = 100;
+        var temphsv;
+        $('#val').change(function(){
+            saturation = this.value;
+        })
+        for (var i = 0; i < c; i++) {
+            var _cp = (100/c)* i;
+            var temprgb = hsv2hex(100, _cp , 100);
+            addRadii(
+                (localStorage['radii' + i] || .97), 
+                (localStorage['speed' + i ] || (Math.random() * .1) ),
+                false,
+                temprgb
+                );
+        };
+        
     });
 }
--- main.js
+++ main.js
@@ -1,101 +1,53 @@
+
+function arg(_a, ia, def, returnArray) {
+    var v = null
+
+    // if ia is an array, find the
+    // first correct definition
+    if (ia.constructor  == Array) { 
+        for(var i=0; i<ia.length; i++) {
+            if(_a[ia[i]] || _a[ia[i]] === false ){
+                v = _a[ia[i]];
+                break;
+            }
+        }
+    }
+    else {
+        if(_a[ia] || _a[ia] === false ) v = _a[ia];
+    }
+
+    if( (v == null) && (def != undefined) ) {
+        v = def
+    }
+
+    if(returnArray){
+        return [v, ia[i]]
+    }
+    else
+    {
+        return v
+    }
+
+}
+
+
 var vectors = 4,
 	ready = false,
 	radii = [],
 	speeds = [],
-	toDraw = [];
-
-var fractal = document.getElementById( 'fractal' ),
+	colors= [],
+	toDraw = [],
+	fractal = document.getElementById( 'fractal' ),
 	circles = document.getElementById( 'circles' ),
 	fractalContext = fractal.getContext( '2d' ),
 	circleContext = circles.getContext( '2d' ),
 	width = circles.width,
 	height = circles.height,
 	rotate = [  ],
+	radiiClock =0,
+	posRadClock = true,
 	pX = 0,
-	pY = 0,
-	rCount = 0,
-	r = 255,
-	g = 255,
-	b = 0,
-	rD = -5,
-	gD = -5,
-	bD = 5;
-
-addRadii = function(){
-	var _id = Math.random().toString(32).slice(2);
-	var i = radii.length
-	radii.push(localStorage['sl_' + i] || 1/3);
-	speeds.push(localStorage['sp_' + i] || Math.PI / 300);
-	var ld = String(toDraw.push(0));
-	
-	rotate.push(0)
-	// Make all the draw's 0
-	for (var i = 0; i < toDraw.length; i++) {
-		toDraw[i] = 0;
-	};
-	// draw the last
-	toDraw[ld] = 1;
-	var index =ld-1;
-
-	
-
-	var st= "<div><span data-uid='" + _id + "' data-for='slider" + ld + "'>" +  index + "</span>"
-	st += "<div class='sl' data-uid='" + _id +"' id=slider" + ld + "></div></div>"
-
-	var $st = $('#sliders').append(st);
-
-	function onChange( ev, ui ){ 
-		radii[ index + 1 ] = ui.value / 100; 
-		localStorage['sl_' + index] = (ui.value * 3) /300;
-		$(this).parent().parent().find('span[data-uid="' + $('#slider' + index).data('uid') + '"]').text(localStorage['sl_' + index])
-	}
-
-	function onSpeed( ev, ui ){
-		speeds[ index ] = 5* ( ( (ui.value * 4 ) / 400 ) - .5 ); 
-		localStorage['sp_' + index] = speeds[ index ];
-		$(this).parent().find('span[data-for="' + $(this).attr('id') + '"]').text(localStorage['sp_' + index] )
-
-	}
-	
-	
-
-	$('span[data-uid="' + _id + "\"]").text(radii[index])
-
-	$( '<div class="tl"></div>' ).appendTo( '.sl[data-uid="' + _id +'"]' ).slider({
-		orientation: 'horizontal',
-		animate: true,
-		value: 100 * (localStorage['sl_' + i] || radii[ i]),
-		slide: onChange,
-		change: onChange,
-		step: .01
-	});
-
-	var spid = Math.random().toString(32).slice(2);
-	var sp = "<span data-uid='" + spid + "' data-for='speed" + index + "'>Text</span><div class='sl' data-uid='" + spid + "' id=speed" + index + "></div>"
-	$('#speeds').append(sp);
-	
-	$('.sl[data-uid="'+ spid + '"]').slider({
-		orientation : 'horizontal',
-		animate : true,
-		value : 50 + ( 100 * (localStorage['sp_' + index] || speeds[ index ]) / 5 ),
-		slide : onSpeed,
-		change : onSpeed,
-		step : .01,
-		max: 50 + 5,
-		min: 50 - 5
-	});
-} 
-
-addRadii(null)
-
-
-
-fractalContext.strokeStyle = '#222';
-fractalContext.fillStyle   = 'rgba(51, 51, 51, .05 )';
-fractalContext.lineWidth   = 2;
-
-circleContext.strokeStyle = '#eee';
-circleContext.lineWidth   = 2;
+	pY = 0;
 
 window.requestAnimFrame = (function(){
 	return  window.requestAnimationFrame       ||
@@ -108,34 +60,101 @@ window.requestAnimFrame = (function(){
 
 (function animloop(){
 	requestAnimFrame(animloop);
-	if(ready)
 	render();
 })();
+
+changeRadii = function changeRadii(v){
+	for (var i = 0; i < radii.length; i++) {	
+		radii[i]= v
+	};
+}
 
 function render(){
 	var x = width  / 2,
 		y = height / 2,
 		r = Math.min( width, height ) / 2,
 		r2;
-	
 
+	circleContext.lineWidth = 2;
 	// clear the circles
 	circleContext.clearRect( 0, 0, width, height );
 
 	// draw one circle
-	function drawCircle( ix ){
+	function drawCircle(i){
 		circleContext.beginPath();
+		circleContext.strokeStyle = colors[i]
 		circleContext.arc( x, y, r, 0, 2 * Math.PI, false );
 		circleContext.stroke();
 	}
 
+	//if(radiiClock > 400 || radiiClock < 0) posRadClock = !posRadClock;
+	/*
+	for (var i = 0; i < radii.length; i++) {	
+		radii[i]= .980 + (.0001 * radiiClock/2)
+	};
+
+	(posRadClock)? radiiClock++: radiiClock--
+	*/
+
 	for( var i=0; i < toDraw.length; i++ ){
 		
-		drawCircle( i );
-		r2 = r * (radii[i+1]) || 1/3; // shrink each circle
+		drawCircle(i);
+		r2 = r * radii[i] || 1/3; // shrink each circle
 		rotate[ i ] += .1 * speeds[ i ] * 10;
 		x += ( r - r2 ) * Math.cos( rotate[ i ] );
 		y += ( r - r2 ) * Math.sin( rotate[ i ] );
 		r = r2;
 	}
+
 }
+
+
+
+addRadii = function(){
+	var _color 		= Math.round((Math.random() * (0xFFFFFF - 0) + 0)).toString(16),
+		radius 		= arg(arguments, 0, 1/3),
+		speed 		= arg(arguments, 1, Math.PI / 300),
+		controls 	= arg(arguments, 2, true),
+		color 		= arg(arguments, 3, _color);
+
+	
+ 	var r = radii.push(radius) - 1;
+	speeds.push(speed);
+	
+	colors.push(color)
+
+	var ld = String(toDraw.push(0)) -1;
+	ld + 1;
+	rotate.push(0)
+	var index = ld;
+	var _id = Math.random().toString(32).slice(2);
+
+	if(!controls) return r;
+
+	var slider1 = '<input data-index="' +  index 
+		+ '"  data-r="' +  r
+		+ '" id="radius_' + _id
+		+ '" type="range" min="' + 1 
+		+ '" max="' + 99
+		+ '" />';
+
+	var slider2 = '<input data-index="' +  index 
+		+ '"  data-r="' +  r 
+		+ '" id="speed_' + _id 
+		+ '" type="range" min="' + -100
+		+ '" max="' + 100
+		+ '" />';
+	
+	$(slider1).appendTo('#sliders').change(function(){
+		var _r = $(this).data('r') - 1;
+		localStorage['radii' + _r ] = radii[ _r ] = this.value / 100; 
+	})
+
+	$(slider2).appendTo('#speeds').change(function(){
+		var _r = $(this).data('r') - 1;
+		localStorage['speed' + _r ] = speeds[ _r ] = ( this.value / 3) * .005;
+	})
+
+	return r
+}
+
